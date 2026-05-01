d54f9e1a93c1cf13abbb4563c941daa19177aa5a
Volker Christian <me@vchrist.at>
2026-05-01 20:45:35 +0200

    Add turbidity module header

---
+++ LoRaWAN-HelloWorld-radiolib/modules/turbidity-analog/TurbiditySensor.h
@@ -0,0 +1,38 @@
+#ifndef TURBIDITY_SENSOR_MODULE_H
+#define TURBIDITY_SENSOR_MODULE_H
+
+#include <Arduino.h>
+
+namespace turbidity {
+
+class TurbiditySensor {
+public:
+    TurbiditySensor(uint8_t pin,
+                    float vcc,
+                    float adcMax,
+                    float clearVoltage,
+                    float clearNTU,
+                    float turbidVoltage,
+                    float turbidNTU);
+
+    void setup();
+    float readVoltage();
+    float getNTU(float temperatureC = 25.0f);
+
+private:
+    uint8_t pin;
+    float vcc;
+    float adcMax;
+
+    float clearVoltage;
+    float clearNTU;
+    float turbidVoltage;
+    float turbidNTU;
+
+    float a;
+    float b;
+};
+
+} // namespace turbidity
+
+#endif // TURBIDITY_SENSOR_MODULE_H
